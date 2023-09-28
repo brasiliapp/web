2cae3a6d5c3f2c5284237ef4ff185b751a5b8a04
Victor Campos <campos.viictor@gmail.com>
2023-09-28 17:13:39 -0300

    refactor: default layout file in app router & set default metadata api

--- app/layout.js
+++ app/layout.js
@@ -1,36 +1,58 @@
-import "./globals.css";
+import Script from "next/script";
 import { Inter } from "next/font/google";
-import Head from "next/head";
-import { GA_TRACKING_ID, pageview } from "../utils/analytics";
 
-const inter = Inter({ subsets: ["latin"] });
+import Header from "@/components/Header";
+import Footer from "./components/Footer";
+
+import { GA_TRACKING_ID } from "../utils/analytics";
+
 import { defaultSeoConfig } from "@/seoConfig";
 
+import "./globals.css";
+
+const { title, description } = defaultSeoConfig;
+
 export const metadata = {
-  title: defaultSeoConfig.title,
-  description: defaultSeoConfig.description,
+  metadataBase: new URL("https://brasiliapp.com.br"),
+  title,
+  description,
+  openGraph: {
+    images: [
+      {
+        url: "/gastos-com-cota-parlamentar.png",
+        title,
+        description,
+      },
+    ],
+  },
+  alternates: {
+    canonical: "/",
+  },
 };
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <Head>
-        <title>{defaultSeoConfig.title}</title>
-        <meta name="description" content={defaultSeoConfig.description} />
-        <meta property="og:title" content={defaultSeoConfig.title} />
-        <meta
-          property="og:description"
-          content={defaultSeoConfig.description}
-        />
-        <meta property="og:image" content={defaultSeoConfig.imageUrl} />
-        <link rel="canonical" href={`https://brasiliapp.com.br/`} />
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+      <body className={inter.className}>
+        <Header />
+
+        <main className="w-full max-w-screen-xl mx-auto">{children}</main>
+
+        <Footer />
+      </body>
+
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+
+      <Script
+        id="analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -39,15 +61,14 @@ export default function RootLayout({ children }) {
               page_path: window.location.pathname,
             });
           `,
-          }}
-        />
-           <script
-          id="stripe"
-          async
-          src="https://js.stripe.com/v3/buy-button.js"
-        />
-      </Head>
-      <body className={inter.className}>{children}</body>
+        }}
+      />
+
+      <Script
+        id="stripe"
+        strategy="lazyOnload"
+        src="https://js.stripe.com/v3/buy-button.js"
+      />
     </html>
   );
 }
