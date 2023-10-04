6e34607c201e94e9d52a7e1b9f438c0565f8ff3f
SamuraX <matheusomr1@gmail.com>
2023-10-04 16:17:27 -0300

    refactor(/deputado-federal/[name]): extract videos tab from page component
    The VideosTab component was created as a product of this extraction. The components that are only used by it are now on it's same directory. The new directory location for the VideosTab and it's children components is: components/deputado-federal/[name]/tabs/videos.

--- components/deputado-federal/[name]/index.js
+++ components/deputado-federal/[name]/index.js
@@ -1,7 +1,9 @@
 import { ExpensesTab } from "./tabs/expenses/ExpensesTab";
 import { CabinetTab } from "./tabs/cabinet/CabinetTab";
+import { VideosTab } from "./tabs/videos/VideosTab"
 
 export const tabs = {
   Expenses: ExpensesTab,
-  Cabinet: CabinetTab
+  Cabinet: CabinetTab,
+  Videos: VideosTab,
 };
--- pages/deputado-federal/[name].js
+++ pages/deputado-federal/[name].js
@@ -284,44 +284,7 @@ export default function FederalDeputy({ data }) {
             <Tab key="videos" title="Videos">
               <Card>
                 <CardBody>
-                  {data?.speechesData?.length === 0 && (
-                    <>
-                      <small className="text-start">
-                        Não existe vídeos para esse parlamentar
-                      </small>
-                    </>
-                  )}
-                  {data?.speechesData?.length > 0 && (
-                    <>
-                      <div
-                        className="bg-gray-100 mb-5 border-l-4 border-gray-300 text-dark-700 p-4 rounded-lg"
-                        role="alert"
-                      >
-                        Os vídeos abaixo foram capturados pela página do
-                        deputado no site da câmara dos deputados, se não
-                        conseguir visualizar aqui clique no link que redireciona
-                        para a página da câmara.
-                      </div>
-                      <ol className="relative border-l border-gray-200 dark:border-gray-700">
-                        {data?.speechesData.map((speeche) => {
-                          return (
-                            <EventItem
-                              key={speeche.evento_id}
-                              id={speeche.evento_id}
-                              title={speeche.event_title}
-                              date={speeche.date}
-                              time={speeche.time}
-                              place={speeche.place}
-                              videos={speeche.video_links}
-                              name={
-                                data?.federalDeputyBaseInfo?.ultimoStatus?.nome
-                              }
-                            />
-                          );
-                        })}
-                      </ol>
-                    </>
-                  )}
+                  <tabs.Videos speechesData={data?.speechesData} federalDeputyName={data?.federalDeputyBaseInfo?.ultimoStatus?.nome}/>
                 </CardBody>
               </Card>
             </Tab>
