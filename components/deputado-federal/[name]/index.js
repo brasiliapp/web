24d6870a36c28d9b30f26a34494fc5b4f56146c7
SamuraX <matheusomr1@gmail.com>
2023-10-04 16:30:56 -0300

    refactor(/deputado-federal/[name]): extract personal info tab from page component
    The PersonalInfoTab component was created as a product of this extraction. It's directory location is: components/deputado-federal/[name]/tabs/personal-info.

--- components/deputado-federal/[name]/index.js
+++ components/deputado-federal/[name]/index.js
@@ -1,9 +1,11 @@
 import { ExpensesTab } from "./tabs/expenses/ExpensesTab";
 import { CabinetTab } from "./tabs/cabinet/CabinetTab";
 import { VideosTab } from "./tabs/videos/VideosTab"
+import { PersonalInfoTab } from "./tabs/personal-info/PersonalInfoTab";
 
 export const tabs = {
   Expenses: ExpensesTab,
   Cabinet: CabinetTab,
   Videos: VideosTab,
+  PersonalInfo: PersonalInfoTab,
 };
